7640404ceaebeee41c81442535bdc72d2d167fe8
Yazan Al-Yasin <yalyasin@cloudera.com>
2024-01-30 23:38:40 +0400

    Add GCP provider

---
+++ setup/terraform/providers/gcp/instances.tf
@@ -0,0 +1,250 @@
+# centos boot script on GCP
+locals {
+  init_script = <<-EOF
+  #!/bin/bash
+  # gcp cloud image post-fix script
+  # - set hostname
+  # - add sudo user
+  # - allow root access
+  SUDO_USER='%s'
+  CLUSTER_HOSTNAME='%s'
+  AUTH_KEYS='%s'
+  hostnamectl set-hostname $CLUSTER_HOSTNAME --static
+  CONF_FILE="/etc/workshop.conf"
+  if [[ ! -f $CONF_FILE ]]; then
+    #initialization on first boot
+    useradd -G 4,39,1000 $SUDO_USER
+    mkdir /home/$SUDO_USER/.ssh
+    touch /home/$SUDO_USER/.ssh/authorized_keys
+    chmod 0600 /home/$SUDO_USER/.ssh/authorized_keys
+    chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.ssh
+    echo $AUTH_KEYS >> /home/$SUDO_USER/.ssh/authorized_keys
+    sed -i.bak 's/PermitRootLogin *no/PermitRootLogin yes/' /etc/ssh/sshd_config
+    echo "export CLUSTER_ID=%s" >> $CONF_FILE
+  fi
+  EOF
+}
+
+# cluster block
+resource "google_compute_instance" "cluster" {
+  count                   = var.cluster_count
+  name                    = "${var.owner}-${var.name_prefix}-cluster-${count.index}"
+  machine_type            = var.cluster_instance_type
+  zone                    = "${var.gcp_region}-${var.gcp_az}"
+  metadata_startup_script = format(local.init_script,var.ssh_username,"cdp.${google_compute_address.cluster-public-ip[count.index].address}.nip.io",file(var.ssh_public_key), count.index)
+  hostname                = "cdp.${google_compute_address.cluster-public-ip[count.index].address}.nip.io"
+
+  tags = [
+    "${var.owner}-${var.name_prefix}-private-access",
+    "${var.owner}-${var.name_prefix}-workshop-cross-access",
+    "${var.owner}-${var.name_prefix}-cluster-access",
+  ]
+
+  boot_disk {
+    initialize_params {
+      image = var.cluster_ami
+      size  = 200
+      type  = "pd-balanced"
+    }
+    auto_delete = true
+  }
+
+  attached_disk {
+    source      = google_compute_disk.cluster-pd[count.index].self_link
+    device_name = "${var.owner}-${var.name_prefix}-cluster-pd-${count.index}-disk-0"
+    mode        = "READ_WRITE"
+  }
+
+  metadata = {
+    ssh-keys = "${var.owner}:${file(var.ssh_public_key)}"
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.subnet1.name
+
+    access_config {
+      nat_ip = google_compute_address.cluster-public-ip[count.index].address
+    }
+  }
+
+  timeouts {
+    create = "10m"
+  }
+
+}
+
+resource "google_compute_disk" "cluster-pd" {
+  count = var.cluster_count
+  name  = "${var.owner}-${var.name_prefix}-cluster-pd-${count.index}"
+  type  = "pd-balanced"
+  zone  = "${var.gcp_region}-${var.gcp_az}"
+  size  = 200
+}
+
+resource "google_compute_address" "cluster-public-ip" {
+  count        = var.cluster_count
+  name         = "${var.owner}-${var.name_prefix}-cluster-public-ip-${count.index}"
+  address_type = "EXTERNAL"
+}
+
+# web block
+resource "google_compute_instance" "web" {
+  count                   = (var.launch_web_server ? 1 : 0)
+  name                    = "${var.owner}-${var.name_prefix}-web"
+  machine_type            = "e2-standard-2"
+  zone                    = "${var.gcp_region}-${var.gcp_az}"
+  metadata_startup_script = format(local.init_script,var.ssh_username,"web.${google_compute_address.web-public-ip[count.index].address}.nip.io",file(var.web_ssh_public_key), count.index)
+  hostname                = "web.${google_compute_address.web-public-ip[count.index].address}.nip.io"
+
+  tags = [
+    "${var.owner}-${var.name_prefix}-private-access",
+    "${var.owner}-${var.name_prefix}-web-access",
+  ]
+
+  boot_disk {
+    initialize_params {
+      image = var.cluster_ami
+      size  = 20
+      type  = "pd-balanced"
+    }
+    auto_delete = true
+  }
+
+  metadata = {
+    ssh-keys = "${var.owner}:${file(var.web_ssh_public_key)}"
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.subnet1.name
+
+    access_config {
+      nat_ip = google_compute_address.web-public-ip[count.index].address
+    }
+  }
+
+  timeouts {
+    create = "10m"
+  }
+
+}
+
+resource "google_compute_address" "web-public-ip" {
+  count        = (var.launch_web_server ? 1 : 0)
+  name         = "${var.owner}-${var.name_prefix}-web-public-ip-${count.index}"
+  address_type = "EXTERNAL"
+}
+
+# ipa block
+resource "google_compute_instance" "ipa" {
+  count                   = (var.use_ipa ? 1 : 0)
+  name                    = "${var.owner}-${var.name_prefix}-ipa"
+  machine_type            = "e2-standard-2"
+  zone                    = "${var.gcp_region}-${var.gcp_az}"
+  metadata_startup_script = format(local.init_script,var.ssh_username,"ipa.${google_compute_address.ipa-public-ip[count.index].address}.nip.io",file(var.ssh_public_key), count.index)
+  hostname                = "ipa.${google_compute_address.ipa-public-ip[count.index].address}.nip.io"
+
+  tags = [
+    "${var.owner}-${var.name_prefix}-private-access",
+    "${var.owner}-${var.name_prefix}-workshop-cross-access",
+    "${var.owner}-${var.name_prefix}-cluster-access",
+  ]
+
+  boot_disk {
+    initialize_params {
+      image = var.cluster_ami
+      size  = 20
+      type  = "pd-balanced"
+    }
+    auto_delete = true
+  }
+
+  metadata = {
+    ssh-keys = "${var.owner}:${file(var.ssh_public_key)}"
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.subnet1.name
+
+    access_config {
+      nat_ip = google_compute_address.ipa-public-ip[count.index].address
+    }
+  }
+
+  timeouts {
+    create = "10m"
+  }
+
+}
+
+
+resource "google_compute_address" "ipa-public-ip" {
+  count        = (var.use_ipa ? 1 : 0)
+  name         = "${var.owner}-${var.name_prefix}-ipa-public-ip-${count.index}"
+  address_type = "EXTERNAL"
+}
+
+# ecs block
+resource "google_compute_instance" "ecs" {
+  count                   = (var.pvc_data_services ? var.cluster_count : 0)
+  name                    = "${var.owner}-${var.name_prefix}-ecs-${count.index}"
+  machine_type            = var.ecs_instance_type
+  zone                    = "${var.gcp_region}-${var.gcp_az}"
+  metadata_startup_script = format(local.init_script,var.ssh_username,"ecs.${google_compute_address.ecs-public-ip[count.index].address}.nip.io",file(var.ssh_public_key), count.index)
+  hostname                = "ecs.${google_compute_address.ecs-public-ip[count.index].address}.nip.io"
+
+  tags = [
+    "${var.owner}-${var.name_prefix}-private-access",
+    "${var.owner}-${var.name_prefix}-workshop-cross-access",
+    "${var.owner}-${var.name_prefix}-cluster-access",
+  ]
+
+  boot_disk {
+    initialize_params {
+      image = var.ecs_ami
+      size  = 500
+      type  = "pd-balanced"
+    }
+    auto_delete = true
+  }
+
+
+  metadata = {
+    ssh-keys = "${var.owner}:${file(var.ssh_public_key)}"
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.subnet1.name
+
+    access_config {
+      nat_ip = google_compute_address.ecs-public-ip[count.index].address
+    }
+  }
+
+
+  timeouts {
+    create = "10m"
+  }
+
+}
+
+resource "google_compute_address" "ecs-public-ip" {
+  count        = (var.pvc_data_services ? var.cluster_count : 0)
+  name         = "${var.owner}-${var.name_prefix}-ecs-public-ip-${count.index}"
+  address_type = "EXTERNAL"
+}
+
+# GCP instance group
+resource "google_compute_instance_group" "servers" {
+  name        = "${var.owner}-${var.name_prefix}-servers"
+  description = "${var.project}"
+
+  instances = flatten([
+    [for id in (google_compute_instance.cluster.*.self_link): id],
+    [for id in (google_compute_instance.ecs.*.self_link): id],
+    [for id in (google_compute_instance.web.*.self_link): id],
+    [for id in (google_compute_instance.ipa.*.self_link): id],
+  ])
+
+  zone = "${var.gcp_region}-${var.gcp_az}"
+}
+
